6951a2a8337570de9ae6a9fe7f17636d7e17df79
maciekMerkury <maciekmerkury2@gmail.com>
2021-05-07 18:27:42 +0200

    fish config update featuring fix for some crappy thing that happens on my laptop for some reason and, on more important note, exporting .cargo/bin to PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,24 @@
 #!/bin/fish
 
+# shut up the greeting
 set fish_greeting ""
+
+# adds the binaries installed by "cargo install" to path
+set -xa PATH ~/.cargo/bin
+
 
 # sources aliases
 source ~/.config/fish/aliases.fish
 
 # source functions
 source ~/.config/fish/functions.fish
+
+# it's a dirty fix for the crappy directory being created in home
+# $1 == 1 -> prints pwd
+# $1 == 2 -> prints a message if directory does not exist
+# $1 == 3 -> 1 & 2 combined
+# default($1) -> prints nothing
+/home/maciek/Programming/Shells/quick-fix-tm.bash
 
 # start starship prompt
 #export STARSHIP_CONFIG=~/.config/starship/
